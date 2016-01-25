c5b43b5b04076a24fd5ecd3f8390edae240a85f5
Jorge Sánchez Fernández <xurxosf@gmail.com>
2016-01-25 13:03:18 +0100

    Add repository example

--- CleanArchitectureDemo/CleanArchitectureDemo.Infrastructure.Data/Models/ProductEF.cs
+++ CleanArchitectureDemo/CleanArchitectureDemo.Infrastructure.Data/Models/ProductEF.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchitectureDemo.Infrastructure.Data
 {
-    public class Class1
+    public class ProductEF
     {
     }
 }
---
+++ CleanArchitectureDemo/CleanArchitectureDemo.Infrastructure.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitectureDemo.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitectureDemo.Domain.Entities;
+
+namespace CleanArchitectureDemo.Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public List<Product> Get()
+        {
+            //Retrieve productsEF
+
+            //Map to domain products
+
+            //return domain products
+            throw new NotImplementedException();
+        }
+
+        public Product GetById()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
