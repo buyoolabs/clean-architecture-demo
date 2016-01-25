04dee67ead33728a2728623c3cef8c388864ea4d
Javier Angel Martinez Rodriguez <xxavimr@gmail.com>
2016-01-25 11:53:03 +0100

    Include GetListProductsResponse in project

---
+++ CleanArchitectureDemo/CleanArchitectureDemo.Entities/UseCases/GetListProducts/GetListProductsResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureDemo.Domain.UseCases.GetListProducts
+{
+    public class GetListProductsResponse
+    {
+        public List<ProductsItem> Products;
+        public GetListProductsResponse()
+        {
+            Products = new List<ProductsItem>();
+        }
+    }
+
+    public class ProductsItem
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
